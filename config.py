040f49343ea48c95568d09c4e7c08958ce184cdb
Antonov Maksim <goodmax1234567@mail.ru>
2023-03-23 00:47:23 +0500

    Added step by step guide

--- config.py
+++ config.py
@@ -2,6 +2,8 @@
 # Proxy
 """
 !!! Use a proxy so that you are not blocked from the server side. !!!
+
+See the installation instructions in the project repository in the settings/proxy folder.
 """
 
 IP = 'YOUR IP'
@@ -25,27 +27,9 @@ USER_PAGINATION = 1
 # Link to your country page with the specified year
 GLOBAL_URL = [
     'https://www.kinopoisk.ru/lists/movies/country--1/year--',
-    #'https://www.kinopoisk.ru/lists/movies/country--2/year--',
-    #'https://www.kinopoisk.ru/lists/movies/country--8/year--',
-    #'https://www.kinopoisk.ru/lists/movies/country--14/year--',
-    #'https://www.kinopoisk.ru/lists/movies/country--15/year--',
-    #'https://www.kinopoisk.ru/lists/movies/country--11/year--',
-    #'https://www.kinopoisk.ru/lists/movies/country--3/year--',
-    #'https://www.kinopoisk.ru/lists/movies/country--26/year--',
-    #'https://www.kinopoisk.ru/lists/movies/country--9/year--',
-    #'https://www.kinopoisk.ru/lists/movies/country--31/year--',
 ]
 
 # Country
 COUNTRY = [
     'США',
-    #'Россия',
-    #'Франция',
-    #'Италия',
-    #'Испания',
-    #'Великобритания',
-    #'Германия',
-    #'Корея Южная',
-    #'Япония',
-    #'Китай',
 ]
