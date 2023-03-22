193d3b64ddf13c06746b55eec946ca2d3acf33f0
Antonov Maksim <goodmax1234567@mail.ru>
2023-03-22 23:46:51 +0500

    Fix language

--- functions.py
+++ functions.py
@@ -5,5 +5,5 @@ def timingDecorator(function):
     def wrapper():
         time_start = time.time()
         function()
-        print(f'\nВремя выполнения составило: {round(time.time() - time_start, 2)}')
+        print(f'\nThe execution time was: {round(time.time() - time_start, 2)}')
     return wrapper
